1c1ca6e9ce11c93ac332ec4b68198f33e2db6f5b
Nguyễn Tuấn Nghĩa <hiimnghia.real@gmail.com>
2024-09-04 05:41:33 +0700

    ui change

--- src/pages/Settings/layout.tsx
+++ src/pages/Settings/layout.tsx
@@ -18,7 +18,7 @@ export default function SettingLayout({ children }: React.PropsWithChildren) {
     <div className='pc:rounded-xl flex flex-col bg-tier0 h-dvh min-h-dvh w-dvw text-tier0 overflow-auto'>
 
       {/* ANIMATION FRAME */}
-      <div ref={ref} className='absolute bg-tier2 h-dvh w-dvw top-0 left-0 pc:rounded-xl animate-fade-out'></div>
+      <div ref={ref} className='absolute bg-tier2 h-dvh w-dvw top-0 left-0 pc:rounded-xl animate-fade-out z-50'></div>
 
       {/* INIT FRAME */}
       <div className='m-auto bg-tier0 py-4 w-[350px] h-[750px] pc:border-2 pc:rounded-xl pc:border-tier3 mobile:w-full mobile:h-full'>
@@ -26,10 +26,10 @@ export default function SettingLayout({ children }: React.PropsWithChildren) {
         <div className='flex flex-col m-auto w-full h-full'>
           <div className='flex flex-row text-center mx-auto relative w-full'>
             <SVGBackArrow className='w-6 h-6 absolute left-1 top-1/2 -translate-y-1/2 cursor-pointer' onClick={() => navigate(-1)} />
-            <div className='font-bold text-2xl text-tier9 text-center m-auto'>{location.pathname.split('/')[2]}</div>
+            <div className='font-bold text-2xl text-tier0 text-center m-auto'>{location.pathname.split('/')[2]} Settings</div>
           </div>
 
-          <div className='m-auto mt-2 flex flex-col w-full h-full'>
+          <div className='m-auto mt-5 flex flex-col w-full h-full'>
             {children}
           </div>
         </div>
--- src/pages/Settings/microphone.tsx
+++ src/pages/Settings/microphone.tsx
@@ -16,12 +16,22 @@ export default function Microphone() {
     setList([...list, inputRef.current!.value])
   }
 
+  const onClick = () => {
+    setTimeout(() => inputRef.current?.scrollIntoView({ behavior: 'smooth' }), 50)
+  }
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    inputRef.current?.scrollIntoView({ behavior: 'instant' })
+    if (e.key === 'Enter') addList()
+  }
+
   return (
     <SettingLayout>
-      <div className='h-full w-full gap-3 flex flex-col relative'>
+      <span className='mx-auto font-bold text-xl'>Program List</span>
 
+      <div className='h-full w-full gap-3 flex flex-col relative'>
         {/* Application list */}
-        <div className='flex flex-col bg-tier2 w-72 h-px550 mx-auto rounded-lg'>
+        <div className='flex flex-col bg-tier2 w-72 h-px500 mx-auto rounded-lg'>
           <div className='inline-flex flex-col w-full h-full overflow-auto align-top'>
             {list.map(item => (
               <div className='flex flex-row text-lg my-1 mx-2 border-b-tier4 [&:not(:last-child)]:border-b-2'>
@@ -34,18 +44,21 @@ export default function Microphone() {
         </div>
 
         {/* Button list */}
-        <div className='w-72 h-[8%] flex mx-auto gap-2'>
-          <div className='bg-tier2 rounded-lg h-full w-[79%] flex'>
+        <div className='w-72 h-[54px] flex mx-auto gap-2'>
+          <div className='bg-tier2 rounded-lg h-full w-[226px] flex mx-auto'>
             <input
               className='w-4/5 m-auto bg-transparent outline-none border-b-2 border-b-tier4 text-center'
               ref={inputRef}
               type='text'
               placeholder='Add more programs'
-              onKeyDown={e => e.key === 'Enter' && addList()}
+              autoComplete='off'
+              autoCorrect='off'
+              onClick={onClick}
+              onKeyDown={onKeyDown}
             />
           </div>
           <div
-            className='cursor-pointer bg-tier2 rounded-lg h-full w-[54px] flex'
+            className='cursor-pointer bg-tier2 rounded-lg h-full w-[54px] flex mx-auto'
             onClick={addList}
           >
             <SVGPlus className='w-5 h-5 m-auto' />
@@ -53,7 +66,7 @@ export default function Microphone() {
         </div>
 
         {/* Notice */}
-        <div className='flex flex-col bottom-0 absolute left-1/2 transform -translate-x-1/2 w-full'>
+        <div className='flex flex-col w-full mt-2'>
           <span className='text-sm text-center'>Use "Name" in Task Manager "Details" tab</span>
           <span className='text-sm text-center'>Eg: discord.exe, msedge.exe, code.exe</span>
         </div>
